cd4a4bb02a9d868e39f896d206d64cede8138a8b
Karolina Pristupa <karolina.pr.8@mail.ru>
2025-11-13 02:28:01 +0300

    feat: add enterprise registration

---
+++ src/pages/register/index.module.scss.d.ts
@@ -0,0 +1,8 @@
+declare const classNames: {
+  readonly page: "page";
+  readonly background: "background";
+  readonly container: "container";
+  readonly stepNumber: "stepNumber";
+  readonly animatedText: "animatedText";
+};
+export = classNames;
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import EnterpriseRegisterForm from '@components/enterprise-register-form';
+
+import styles from './index.module.scss';
 
 const Register: React.FC = () => {
-  return <></>;
+  return (
+    <div className={styles.background}>
+      <span className={styles.stepNumber}>01</span>
+      <div className={styles.container}>
+        <h1 className={styles.animatedText}>Зарегистрируйте свою компанию</h1>
+        <EnterpriseRegisterForm />
+      </div>
+    </div>
+  );
 };
 
 export default Register;
